0cfd868b0f359b28135f66f3e408cf1e62655067
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2019-11-15 16:23:55 +1100

    start copying the terrform code across.

--- provisioner.go
+++ provisioner.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
@@ -29,11 +30,24 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 	return nil
 }
 
-func (p *Provisioner) Provision(_ context.Context, ui packer.Ui, _ packer.Communicator) error {
+func (p *Provisioner) Provision(_ context.Context, ui packer.Ui, comm packer.Communicator) error {
 	if p.config.SendToUi {
 		ui.Say(p.config.Comment)
-
+	}
+	stagingDir := "/tmp/packer-terraform"
+	if err := p.uploadDirectory(ui, comm, stagingDir, p.config.CodePath); err != nil {
+		return fmt.Errorf("Error uploading code: %s", err)
 	}
 
 	return nil
+}
+
+func (p *Provisioner) uploadDirectory(ui packer.Ui, comm packer.Communicator, dst string, src string) error {
+	// Make sure there is a trailing "/" so that the directory isn't
+	// created on the other side.
+	if src[len(src)-1] != '/' {
+		src = src + "/"
+	}
+
+	return comm.UploadDir(dst, src, nil)
 }
