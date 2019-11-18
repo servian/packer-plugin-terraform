d4f0c0fb859e25ba70700d3fdaaa4b5c8bcc3b9b
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2019-11-18 14:58:22 +1100

    better output and some felxibility.

--- provisioner.go
+++ provisioner.go
@@ -3,34 +3,46 @@ package main
 import (
 	"bytes"
 	"context"
-	"errors"
 	"fmt"
-	"strings"
 
 	"github.com/hashicorp/packer/helper/config"
 	"github.com/hashicorp/packer/packer"
+	"github.com/hashicorp/packer/provisioner"
+	"github.com/hashicorp/packer/template/interpolate"
 )
 
 // Config struct containing variables
 type Config struct {
-	Version  string `mapstructure:"version"`
-	CodePath string `mapstructure:"code_path"`
-	Comment  string `mapstructure:"comment"`
-	SendToUI bool   `mapstructure:"ui"`
+	Version    string `mapstructure:"version"`
+	CodePath   string `mapstructure:"code_path"`
+	RunCommand string `mapstructure:"run_command"`
+	StagingDir string `mapstructure:"staging_dir"`
+
+	ctx interpolate.Context
 }
 
 // Provisioner is the interface to install and run Terraform
 type Provisioner struct {
-	config Config
+	config        Config
+	guestCommands *provisioner.GuestCommands
 }
 
 // Prepare parses the config and get everything ready
 func (p *Provisioner) Prepare(raws ...interface{}) error {
 	err := config.Decode(&p.config, &config.DecodeOpts{
-		Interpolate: true,
+		Interpolate:        true,
+		InterpolateContext: &p.config.ctx,
 	}, raws...)
 	if err != nil {
 		return err
+	}
+
+	if p.config.StagingDir == "" {
+		p.config.StagingDir = "/tmp/packer-terraform"
+	}
+
+	if p.config.RunCommand == "" {
+		p.config.RunCommand = "cd {{.StagingDir}} \u0026\u0026 /usr/local/bin/terraform init \u0026\u0026 /usr/local/bin/terraform apply -auto-approve"
 	}
 
 	return nil
@@ -38,15 +50,22 @@ func (p *Provisioner) Prepare(raws ...interface{}) error {
 
 // Provision does the work of installing Terraform and running it on the remote
 func (p *Provisioner) Provision(_ context.Context, ui packer.Ui, comm packer.Communicator) error {
-	if p.config.SendToUI {
-		ui.Say(p.config.Comment)
-	}
-	stagingDir := "/tmp/packer-terraform"
-	if err := p.uploadDirectory(ui, comm, stagingDir, p.config.CodePath); err != nil {
+	ui.Say("Provisioning with Terraform...")
+
+	ui.Message("Uploading Code")
+	if err := p.uploadDirectory(ui, comm, p.config.StagingDir, p.config.CodePath); err != nil {
 		return fmt.Errorf("Error uploading code: %s", err)
 	}
 
-	command := "cd /tmp/packer-terraform && /usr/local/bin/terraform init && /usr/local/bin/terraform apply -auto-approve"
+	ui.Message("Running Terraform")
+	p.config.ctx.Data = &Config{
+		StagingDir: p.config.StagingDir,
+	}
+	command, err := interpolate.Render(p.config.RunCommand, &p.config.ctx)
+	if err != nil {
+		return err
+	}
+
 	var out, outErr bytes.Buffer
 	cmd := &packer.RemoteCmd{
 		Command: command,
@@ -59,16 +78,9 @@ func (p *Provisioner) Provision(_ context.Context, ui packer.Ui, comm packer.Com
 	if err := cmd.RunWithUi(ctx, comm, ui); err != nil {
 		return err
 	}
-
-	cmd.Wait()
 	if cmd.ExitStatus() != 0 {
-		ui.Error(out.String())
-		ui.Error(outErr.String())
-		return errors.New("Error bootstrapping converge")
+		return fmt.Errorf("non-zero exit status")
 	}
-
-	ui.Message(strings.TrimSpace(out.String()))
-
 	return nil
 }
 
