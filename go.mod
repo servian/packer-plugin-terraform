f9af4cd8ef4b4938c9381c2bf1c7c810f2945ddf
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2020-02-04 15:06:07 +1100

    Add some badges and formatting.

--- go.mod
+++ go.mod
@@ -4,6 +4,6 @@ go 1.13
 
 require (
 	github.com/hashicorp/hcl/v2 v2.0.0
-	github.com/hashicorp/packer v1.5.0
+	github.com/hashicorp/packer v1.5.1
 	github.com/zclconf/go-cty v1.1.2-0.20191126233707-f0f7fd24c4af
 )
--- provisioner_test.go
+++ provisioner_test.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-  "testing"
+	"testing"
 
-  "github.com/hashicorp/packer/packer"
+	"github.com/hashicorp/packer/packer"
 )
 
 func testConfig() map[string]interface{} {
-  return map[string]interface{}{}
+	return map[string]interface{}{}
 }
 
 func TestProvisioner_Impl(t *testing.T) {
-  var raw interface{}
-  raw = &Provisioner{}
-  if _, ok := raw.(packer.Provisioner); !ok {
-    t.Fatalf("must be a Provisioner")
-  }
+	var raw interface{}
+	raw = &Provisioner{}
+	if _, ok := raw.(packer.Provisioner); !ok {
+		t.Fatalf("must be a Provisioner")
+	}
 }
