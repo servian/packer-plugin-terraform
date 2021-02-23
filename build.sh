ab4820d32b7ad914e7d6288d1ff3af390167d961
Tristan Morgan <tristanmorgan@users.noreply.github.com>
2021-02-23 16:50:33 +1100

    Go-releaser

--- build.sh
+++ build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-go fmt
-golint
-
-export GOOS=darwin
-go build -ldflags='-s -w' -o packer-provisioner-terraform
+rm -rf dist/*
+docker run --rm -it --privileged \
+  -v $PWD:/go/src/github.com/servian/packer-plugin-terraform \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -w /go/src/github.com/servian/packer-plugin-terraform \
+  -e GITHUB_TOKEN \
+  goreleaser/goreleaser release
